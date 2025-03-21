fcdcfdd7027a5e55862fb4492a22ba6e437e0e11
纸船 <2167289297@qq.com>
2025-03-21 20:58:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-// 获取当前文件的路径
-const __filename = fileURLToPath(import.meta.url);
-// 获取当前文件所在的目录
-const __dirname = path.dirname(__filename);
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
-});
+  //base: '/zctools/',
+  plugins: [vue()]
+})
